c92191af24fcbf316d6ad82e6097f5d7abc3a710
Abel Osorio <osorioabel@gmail.com>
2018-08-26 18:54:55 +0200

    [feat]: Add blank QuizApp xcodeproj

---
+++ App/QuizApp/QuizApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  QuizApp
+//
+//  Created by Abel Osorio on 8/26/18.
+//  Copyright © 2018 OS Mobile Lab. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
