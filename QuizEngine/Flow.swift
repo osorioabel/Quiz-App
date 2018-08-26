03a78501d54194560ca4fef2b0eafacca63339df
Abel Osorio <osorioabel@gmail.com>
2018-08-26 16:34:50 +0200

    [feat]: Refactor RouterSpy to use an array instead of two variables

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -22,8 +22,8 @@ class Flow {
     }
 
     func start() {
-        if !questions.isEmpty {
-            router.routeTo(question: "")
+        if let firstQuestion = questions.first {
+            router.routeTo(question: firstQuestion)
         }
     }
 }
--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -14,35 +14,54 @@ class FlowTest: XCTestCase {
     func test_start_withNoQuestions_doesNotRouteToQuestion() {
         let router = RouterSpy()
         let sut = Flow(questions: [], router: router)
+
         sut.start()
         
-        XCTAssertEqual(router.routedQuestionCount, 0)
-    }
-
-    func test_start_withOneQuestions_routesToQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions: ["Q1"], router: router)
-        sut.start()
-
-        XCTAssertEqual(router.routedQuestionCount, 1)
+        XCTAssertEqual(router.routedQuestions, [])
     }
 
     func test_start_withOneQuestions_routesToCorrectQuestion() {
         let router = RouterSpy()
         let sut = Flow(questions: ["Q1"], router: router)
+
         sut.start()
 
-        XCTAssertEqual(router.routedQuestion, "Q1")
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
+    }
+
+    func test_start_withOneQuestions_routesToCorrectQuestion_2() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q2"], router: router)
+
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestions, ["Q2"])
+    }
+
+    func test_start_withTwoQuestions_routesToFirstQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
+    }
+
+    func test_startTwice_withTwoQuestions_routesToFirstQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+
+        sut.start()
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestions, ["Q1", "Q1"])
     }
 
     class RouterSpy: Router {
-        var routedQuestionCount: Int = 0
-        var routedQuestion: String? = nil
+        var routedQuestions: [String] = []
 
         func routeTo(question: String) {
-            routedQuestionCount += 1
-            routedQuestion = question
+            routedQuestions.append(question)
         }
-
     }
 }
